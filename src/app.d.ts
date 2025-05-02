fa6450e71524dafe58a0a0a21eaf960166b9741f
David <davidgonsal@gmail.com>
2025-05-02 23:06:54 +0200

    refactor(types): update Locals interface to use TypedPocketBase instead of PocketBase 🛠️

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,13 +1,15 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 
-import PocketBase from 'pocketbase';
+// import PocketBase from 'pocketbase';
+import { type TypedPocketBase } from '$lib/types/pocketbase-types';
 
 declare global {
 	namespace App {
 		// interface Error {}
 		interface Locals {
-			pb: PocketBase;
+			pb: TypedPocketBase;
+			pbAdmin: TypedPocketBase;
 			user: import('pocketbase').Record | null;
 		}
 
