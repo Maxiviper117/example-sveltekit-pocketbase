16d7dc0b7e6518eadc3739ba5209b86da05a4a37
David <davidgonsal@gmail.com>
2025-05-02 14:46:44 +0200

    refactor(load): simplify load function by removing authentication checks

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,17 +1,5 @@
 import type { LayoutServerLoad } from './$types';
 
-export const load = (async ({ locals }) => {
-	if (!locals.pb.authStore.isValid) {
-		locals.pb.authStore.clear();
-		return {
-			user: null
-		};
-	}
-
-	const user = locals.pb.authStore.record;
-
-	console.log('User:', user);
-	return {
-		user
-	};
+export const load = (async () => {
+	return {};
 }) satisfies LayoutServerLoad;
