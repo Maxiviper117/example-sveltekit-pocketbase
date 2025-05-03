db902f9f2b069c96cef8da676c481c91b491242f
David <davidgonsal@gmail.com>
2025-05-03 11:39:25 +0200

    refactor(auth): update PocketBase client usage in middleware for consistency 🔧

--- src/lib/server/middleware/pocketbaseAuth.ts
+++ src/lib/server/middleware/pocketbaseAuth.ts
@@ -8,31 +8,31 @@ export const pocketbaseHandle: Handle = async ({ event, resolve }) => {
 	const SUPER_ADMIN_PASSWORD = process.env.SUPER_ADMIN_PASSWORD as string;
 
 	// Initialize PocketBase client for this request
-	event.locals.pb = new PocketBase(POCKETBASE_URL);
+	event.locals.pbUser = new PocketBase(POCKETBASE_URL);
 
 	const pbAdmin = new PocketBase(POCKETBASE_URL);
 	await pbAdmin.collection('_superusers').authWithPassword(SUPER_ADMIN_EMAIL, SUPER_ADMIN_PASSWORD);
+	event.locals.pbAdmin = pbAdmin; // Assign the admin client to locals for later use
 
 	// Load session data from 'pb_auth' cookie, if present
-	event.locals.pb.authStore.loadFromCookie(event.request.headers.get('cookie') || '');
-	event.locals.pbAdmin = pbAdmin; // Assign the admin client to locals for later use
+	event.locals.pbUser.authStore.loadFromCookie(event.request.headers.get('cookie') || '');
 
 	try {
 		// If a session exists, try to refresh it; on failure clear the auth store
-		if (event.locals.pb.authStore.isValid) {
-			await event.locals.pb.collection('users').authRefresh();
+		if (event.locals.pbUser.authStore.isValid) {
+			await event.locals.pbUser.collection('users').authRefresh();
 		}
 		// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	} catch (_) {
 		// clear the auth store on failed refresh
-		event.locals.pb.authStore.clear();
+		event.locals.pbUser.authStore.clear();
 	}
 
 	// Resolve runs route logic and actions, e.g., login, which may mutate authStore
 	const response = await resolve(event);
 
 	// Export updated authStore into a Set-Cookie header ('pb_auth') for the client
-	const pb_auth = event.locals.pb.authStore.exportToCookie(); // if logging in, this will be populated with the auth token and user data
+	const pb_auth = event.locals.pbUser.authStore.exportToCookie(); // if logging in, this will be populated with the auth token and user data
 	// console.log('pb_auth:', pb_auth);
 	response.headers.append('set-cookie', pb_auth); // instructs the browser to store the cookie
 
--- src/lib/server/middleware/requireLogin.ts
+++ src/lib/server/middleware/requireLogin.ts
@@ -4,13 +4,13 @@ import { getRequestEvent } from '$app/server';
 export async function requireLogin() {
 	const { locals, url } = getRequestEvent();
 
-	if (!locals.pb.authStore.isValid) {
-		locals.pb.authStore.clear();
+	if (!locals.pbUser.authStore.isValid) {
+		locals.pbUser.authStore.clear();
 		const location = new URL('/login', url.origin);
 		location.searchParams.set('redirectTo', url.pathname);
 
 		redirect(307, location);
 	}
 
-	return locals.pb.authStore.record;
+	return locals.pbUser.authStore.record;
 }
